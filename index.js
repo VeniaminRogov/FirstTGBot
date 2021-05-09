8bb9baeb1a248daece913250fc0eeb65210f44e1
RogovVeniamin <rogov.veniamin@gmail.com>
2021-05-09 12:39:40 +0700

    bot done

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
 const TelegramApi = require('node-telegram-bot-api');
 
+const {gameOption, againOption} = require('./options');
+
 const token = '1894222449:AAHDnO499VoQL9iYKkevcqp24YQrWpbqSCs';
 
 const bot = new TelegramApi(token, {polling: true});
 
 const chats = {};
 
-const gameOption = {
-	reply_markup : JSON.stringify({
-		inline_keyboard: [
-			[{text : '1', callback_data : '1'},{text : '2', callback_data : '2'},{text : '3', callback_data : '3'}],
-			[{text : '4', callback_data : '4'},{text : '5', callback_data : '5'},{text : '6', callback_data : '6'}],
-			[{text : '7', callback_data : '7'},{text : '8', callback_data : '8'},{text : '9', callback_data : '9'}],
-			[{text:'0',callback_data: '0'}],
-		]
-	})
-}
-const againOption = {
-	reply_markup : JSON.stringify({
-		inline_keyboard: [
-			[{text : 'Начать заново', callback_data : '/again'}],
-		]
-	})
+const startGame = async (chatId) =>{
+	await bot.sendMessage(chatId, 'Сейчас я загадаю число от 1 до 9, а ты должен её отгадать)');
+	const randomNumber = Math.floor(Math.random()*10);
+	chats[chatId] = randomNumber;
+	await bot.sendMessage(chatId, 'Отгадай число', gameOption);
 }
 
 const start = () =>{
@@ -45,10 +36,7 @@ const start = () =>{
 			return bot.sendMessage(chatId,`Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`);
 		}
 		if(text === '/game'){
-			await bot.sendMessage(chatId, 'Сейчас я загадаю число от 1 до 9, а ты должен её отгадать)');
-			const randomNumber = Math.floor(Math.random()*10);
-			chats[chatId] = randomNumber;
-			return bot.sendMessage(chatId, 'Отгадай число', gameOption);
+			return startGame(chatId);
 		}
 		return bot.sendMessage(chatId, 'Я тебя не понимаю, попробуй еще раз!');
 	})
@@ -57,7 +45,7 @@ const start = () =>{
 		const data = msg.data;
 		const chatId = msg.message.chat.id;
 		if(data === '/again'){
-
+			return startGame(chatId);
 		}
 		if(data === chats[chatId]){
 			return bot.sendMessage(chatId, `Ты отгадал, это число ${chats[chatId]}`, againOption);
